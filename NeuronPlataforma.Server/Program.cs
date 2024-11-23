6bf2f7bd7f8f38bde7707e189d336da8fc0575d5
Guilherme Campelo <campelo.guilherme@hotmail.com>
2024-11-23 18:04:47 -0300

    Teste

--- NeuronPlataforma.Server/Program.cs
+++ NeuronPlataforma.Server/Program.cs
@@ -3,23 +3,40 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuração do Google Authentication
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = GoogleDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
 })
 .AddGoogle(options =>
- {
-     options.ClientId = builder.Configuration["Google:ClientId"];
-     options.ClientSecret = builder.Configuration["Google:ClientSecret"];
- });
+{
+    options.ClientId = builder.Configuration["Google:ClientId"];
+    options.ClientSecret = builder.Configuration["Google:ClientSecret"];
+});
 
+// Adicionar serviços para os controllers
 builder.Services.AddControllers();
+
+// Adicionar o Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); // Necessário para Swagger UI
 
 var app = builder.Build();
 
+// Usar o middleware de autenticação e autorização
 app.UseAuthentication();
 app.UseAuthorization();
+
+// Adicionar o middleware do Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+}
+
+// Mapear os controllers
 app.MapControllers();
 
+// Executar a aplicação
 app.Run();
