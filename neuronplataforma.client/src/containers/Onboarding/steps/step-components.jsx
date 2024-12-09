73b51b75993fe8f92fd706fe2b58ffca7a49f05a
Guilherme Magalhães <guilherme.maga28@gmail.com>
2024-12-09 16:02:36 -0300

    Atualização no Onboarding

--- neuronplataforma.client/src/containers/Onboarding/steps/step-components.jsx
+++
@@ -1,7 +0,0 @@
-export { SourceStep } from './source-step.jsx'
-export { ReasonStep } from './reason-step.jsx'
-export { GoalStep } from './goal-step.jsx'
-export { NotificationStep } from './notification-step.jsx'
-export { SocialStep } from './social-step.jsx'
-export { PathStep } from './path-step.jsx'
-
